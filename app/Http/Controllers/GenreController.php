67bb3731eafda1bfa8cfbabf4c56b9337e07bcb7
EM <_>
2025-06-22 17:56:15 +0700

    Added Total Books per Genre

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GenreController extends Controller
 {
     public function index()
     {
-        $genres = Genre::all();
+        $genres = Genre::withCount(['books' => function ($query) {
+            $query->where('user_id', Auth::id());
+        }])->get();
+
         return view('genres.index', compact('genres'));
     }
 
--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -4,20 +4,22 @@
 <h2 class="mb-4">Genre List</h2>
 
 <div class="table-responsive">
-    <table class="table table-striped table-bordered w-50 mx-auto">
+    <table class="table table-striped table-bordered w-75 mx-auto">
         <thead class="table-dark">
             <tr>
-                <th class="text-center">Name</th>
+                <th class="text-center">Genre</th>
+                <th class="text-center">Your Books</th>
             </tr>
         </thead>
         <tbody>
             @forelse ($genres as $genre)
                 <tr>
                     <td class="text-center">{{ $genre->name }}</td>
+                    <td class="text-center">{{ $genre->books_count }}</td>
                 </tr>
             @empty
                 <tr>
-                    <td class="text-center text-muted">No genres found.</td>
+                    <td colspan="2" class="text-center text-muted">No genres found.</td>
                 </tr>
             @endforelse
         </tbody>
